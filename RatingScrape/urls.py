e71804c91cca39f4e3d51f05898360cc2b4c2348
sonnyky <sonny_yap@hotmail.com>
2016-04-24 17:58:37 +0900

    change how the page works. - Tabs contain review and rating data. - Tabs are navigated and controlled by Javascript.  -Todo : Refactor such that Get Reviews and Get Ratings buttons are fired when tapped only and not render on landing

--- RatingScrape/urls.py
+++ RatingScrape/urls.py
@@ -4,9 +4,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^ios_app_review_fragment/', views.ios_app_review_fragment, name='ios_app_review_fragment'),
+    url(r'^ios_app_rating_fragment/', views.ios_app_rating_fragment, name='ios_app_rating_fragment'),
     url(r'^delete_entry/(?P<entry_id>\d+)$', views.delete_entry, name='delete_entry'),
     url(r'^delete_review_entry/(?P<review_id>\d+)$', views.delete_review_entry, name='delete_review_entry'),
     url(r'^delete_all_review_entry/', views.delete_all_review_entry, name='delete_all_review_entry'),
     url(r'^get_ratings/', views.get_ratings, name='get_ratings'),
     url(r'^get_reviews/', views.get_reviews, name='get_reviews'),
+    url(r'^get_json/', views.get_json, name='get_json'),
 ]
--- RatingScrape/views.py
+++ RatingScrape/views.py
@@ -1,29 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
+from django.http import JsonResponse
 from django.core.urlresolvers import reverse
 from django.utils import timezone
 from urllib.request import urlopen
 from .models import RatingStars
 from .models import UserReviewComments
 import json
+from django.core import serializers
 import logging
+import requests
+import bs4
 
 # This is for getting app ratings from iTunes
 app_id = 419267350
 content = urlopen("https://itunes.apple.com/lookup?id=" + str(app_id) + "&country=jp").read().decode('utf8')
 content_data = json.loads(content)
 
-# This is for getting user comments
+# This is for getting user comments from iTunes
 user_comments = urlopen("http://itunes.apple.com/jp/rss/customerreviews/id="+str(app_id)+"/json").read().decode("utf8")
 user_comments_data = json.loads(user_comments)
 #print(user_comments_data['feed']['author']['name']['label'])
 #print(user_comments_data['feed']['entry'][1]['author']['name']['label'])
 #print(user_comments_data['feed']['entry'][1]['content']['label'])
 
+#This is for getting app ratings from Google Play
+android_app_id = "com.snkplaymore.android014"
+android_app_data_content = requests.get('https://play.google.com/store/apps/details?id='+android_app_id+'&hl=ja')
+soup = bs4.BeautifulSoup(android_app_data_content.text, "html.parser")
+android_app_rating = soup.find_all('div', attrs={'class': 'score'})
+for item in android_app_rating:
+    print(item.text)
+
 # Create your views here.
 def index(request):
     RatingStars.objects.all()
-    UserReviewComments.objects.all()
     #for key, value in content_data.items():
     #   ob.update_field(key, value)
     #   ob.save(update_fields=content_data.keys())
@@ -31,13 +42,24 @@ def index(request):
     #objects.all() returns a dictionary so we need to return the appropriate object type
     context = dict()
     context['ratings'] = RatingStars.objects.all()
-    context['reviews'] = UserReviewComments.objects.all()
     return render(request, 'RatingScrape/dashboard.html', context)
+
+def ios_app_review_fragment(request):
+    UserReviewComments.objects.all()
+    context = dict()
+    context['reviews'] = UserReviewComments.objects.all()
+    return render(request, 'RatingScrape/ios_app_review_fragment.html', context)
+
+def ios_app_rating_fragment(request):
+    RatingStars.objects.all()
+    context = dict()
+    context['ratings'] = RatingStars.objects.all()
+    return render(request, 'RatingScrape/ios_app_rating_fragment.html', context)
 
 def get_ratings(request):
 
     RatingStars.objects.create(
-        star_number = content_data['results'][0]['averageUserRating'],
+        star_number = content_data['results'][0]['averageUserRatingForCurrentVersion'],
         title = content_data['results'][0]['trackName'],
         text = "Other text",
     )
@@ -61,7 +83,6 @@ def delete_all_review_entry(request):
     return HttpResponseRedirect(reverse("RatingScrape:index"))
 
 def get_reviews(request):
-
     for i in user_comments_data['feed']['entry']:
         if 'author' in i:
             UserReviewComments.objects.create(
@@ -69,5 +90,11 @@ def get_reviews(request):
                 comment = i['content']['label'],
             )
     context = dict()
-    context['reviews'] = UserReviewComments.objects.all()
-    return HttpResponseRedirect(reverse("RatingScrape:index"))
+    #context['reviews'] = UserReviewComments.objects.all()
+    context['reviews'] = serializers.serialize('json', UserReviewComments.objects.all())
+    struct = json.loads(context['reviews'])
+    return HttpResponse(json.dumps(struct), content_type='application/json')
+
+def get_json(request):
+
+    return HttpResponse(json.dumps({'username':"TEST"}), content_type='application/json')
